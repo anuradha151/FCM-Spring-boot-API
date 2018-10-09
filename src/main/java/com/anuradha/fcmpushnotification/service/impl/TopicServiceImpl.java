2b298936c64c26acfb917b064b66a5bf1975b895
anuradha151 <anuranasinghe151@gmail.com>
2018-10-10 01:59:59 +0530

    topic service impl annotated

--- src/main/java/com/anuradha/fcmpushnotification/service/impl/TopicServiceImpl.java
+++ src/main/java/com/anuradha/fcmpushnotification/service/impl/TopicServiceImpl.java
@@ -4,7 +4,9 @@ import com.anuradha.fcmpushnotification.dto.TopicDTO;
 import com.anuradha.fcmpushnotification.service.TopicService;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TopicServiceImpl implements TopicService {
     @Override
     public ResponseEntity<?> subscribe(TopicDTO topicDTO) throws FirebaseMessagingException {
