8b07901ef9bf7bd4eb1ba08640b266bd4769bdc7
anuradha151 <anuranasinghe151@gmail.com>
2018-10-10 02:23:18 +0530

    topic save method implemented

--- src/main/java/com/anuradha/fcmpushnotification/service/impl/TopicServiceImpl.java
+++ src/main/java/com/anuradha/fcmpushnotification/service/impl/TopicServiceImpl.java
@@ -1,10 +1,13 @@
 package com.anuradha.fcmpushnotification.service.impl;
 
 import com.anuradha.fcmpushnotification.dto.TopicDTO;
+import com.anuradha.fcmpushnotification.model.Topic;
+import com.anuradha.fcmpushnotification.repository.TopicRepository;
 import com.anuradha.fcmpushnotification.service.TopicService;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.TopicManagementResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
@@ -13,16 +16,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicServiceImpl implements TopicService {
 
-    
+    private final TopicRepository topicRepository;
+
+    @Autowired
+    public TopicServiceImpl(TopicRepository topicRepository) {
+        this.topicRepository = topicRepository;
+    }
+
     @Override
     @Async
     public ResponseEntity<?> subscribe(TopicDTO topicDTO) throws FirebaseMessagingException {
+
+        if (topicDTO == null) {
+            return new ResponseEntity<>("Topic details unavailable", HttpStatus.BAD_REQUEST);
+        }
+
         TopicManagementResponse response = FirebaseMessaging.getInstance().subscribeToTopic(
                 topicDTO.getDeviceTokens(),
                 topicDTO.getTopic()
         );
 
-
+        Topic save = topicRepository.save(dTOToEntity(topicDTO));
+        if (save == null) {
+            return new ResponseEntity<>("Topic saving failed", HttpStatus.BAD_REQUEST);
+        }
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
@@ -35,5 +52,13 @@ public class TopicServiceImpl implements TopicService {
                 topicDTO.getTopic()
         );
         return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    private Topic dTOToEntity(TopicDTO topicDTO) {
+        Topic topic = new Topic();
+        topic.setTopic(topicDTO.getTopic());
+        topic.setDeviceTokens(topicDTO.getDeviceTokens());
+        return topic;
+
     }
 }
