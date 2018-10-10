f66ed3b5461d694f1304c8d8e83c2acc6eac88a2
anuradha151 <anuranasinghe151@gmail.com>
2018-10-10 11:44:00 +0530

    changes commited

--- src/main/java/com/anuradha/fcmpushnotification/model/Notification.java
+++ src/main/java/com/anuradha/fcmpushnotification/model/Notification.java
@@ -1,8 +1,16 @@
 package com.anuradha.fcmpushnotification.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Date;
 
+@Entity
 public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String title;
     private String topic;
     private String body;
@@ -11,4 +19,82 @@ public class Notification {
     private Date date;
     // Time-To-Live.How long the message will be kept in FCM storage if the target devices are offline.
     private long tTL;
+    private String status;
+
+    public Notification() {
+    }
+
+    public Notification(String title, String topic, String body, String color, Date date, long tTL, String status) {
+        this.setTitle(title);
+        this.setTopic(topic);
+        this.setBody(body);
+        this.setColor(color);
+        this.setDate(date);
+        this.settTL(tTL);
+        this.setStatus(status);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public long gettTL() {
+        return tTL;
+    }
+
+    public void settTL(long tTL) {
+        this.tTL = tTL;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
