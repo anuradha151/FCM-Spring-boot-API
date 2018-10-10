d805281f7f4684be97d3ee3ddd2a13ef203dfe27
anuradha151 <anuranasinghe151@gmail.com>
2018-10-10 11:32:15 +0530

    topic model annotated

--- src/main/java/com/anuradha/fcmpushnotification/model/Topic.java
+++ src/main/java/com/anuradha/fcmpushnotification/model/Topic.java
@@ -1,10 +1,15 @@
 package com.anuradha.fcmpushnotification.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
 public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long topic_id;
     private String topic;
     private List<String> deviceTokens;
