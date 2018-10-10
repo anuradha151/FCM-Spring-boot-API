16f838fc0a5d684b94b8a01609738a7ea79ae68f
anuradha151 <anuranasinghe151@gmail.com>
2018-10-10 11:46:47 +0530

    notification repository changes commited

--- src/main/java/com/anuradha/fcmpushnotification/repository/NotificationRepository.java
+++ src/main/java/com/anuradha/fcmpushnotification/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.anuradha.fcmpushnotification.repository;
 
+import com.anuradha.fcmpushnotification.model.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface NotificationRepository extends JpaRepository {
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
 }
