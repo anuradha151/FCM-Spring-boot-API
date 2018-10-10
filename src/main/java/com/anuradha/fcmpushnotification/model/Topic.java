45a8fd513275ca3511da66d7792dd29b0290d581
anuradha151 <anuranasinghe151@gmail.com>
2018-10-11 01:41:47 +0530

    user model changed

--- src/main/java/com/anuradha/fcmpushnotification/model/Topic.java
+++ src/main/java/com/anuradha/fcmpushnotification/model/Topic.java
@@ -9,16 +9,52 @@ public class Topic {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long topic_id;
     private String topic;
-    @ManyToMany(cascade = { CascadeType.ALL })
+    @ManyToMany(cascade = {CascadeType.ALL})
     @JoinTable(
             name = "topic_user",
-            joinColumns = { @JoinColumn(name = "topic_id") },
-            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+            joinColumns = {@JoinColumn(name = "topic_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
     )
     private List<User> users;
 
     public Topic() {
     }
 
+    public Topic(String topic, List<User> users) {
+        this.setTopic(topic);
+        this.setUsers(users);
+    }
 
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "topic_id=" + getTopic_id() +
+                ", topic='" + getTopic() + '\'' +
+                ", users=" + getUsers() +
+                '}';
+    }
+
+    public long getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(long topic_id) {
+        this.topic_id = topic_id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
 }
