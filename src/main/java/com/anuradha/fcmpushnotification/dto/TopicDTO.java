d6b328668793cc9d8fb045f798f05cccca56dd9c
anuradha151 <anuranasinghe151@gmail.com>
2018-10-11 12:03:22 +0530

    small changes commited

--- src/main/java/com/anuradha/fcmpushnotification/dto/TopicDTO.java
+++ src/main/java/com/anuradha/fcmpushnotification/dto/TopicDTO.java
@@ -11,5 +11,42 @@ public class TopicDTO implements Serializable {
     public TopicDTO() {
     }
 
+    public TopicDTO(long topic_id, String topic, List<UserDTO> userDTOS) {
+        this.setTopic_id(topic_id);
+        this.setTopic(topic);
+        this.setUserDTOS(userDTOS);
+    }
 
+    @Override
+    public String toString() {
+        return "TopicDTO{" +
+                "topic_id=" + getTopic_id() +
+                ", topic='" + getTopic() + '\'' +
+                ", userDTOS=" + getUserDTOS() +
+                '}';
+    }
+
+    public long getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(long topic_id) {
+        this.topic_id = topic_id;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public List<UserDTO> getUserDTOS() {
+        return userDTOS;
+    }
+
+    public void setUserDTOS(List<UserDTO> userDTOS) {
+        this.userDTOS = userDTOS;
+    }
 }
--- src/main/java/com/anuradha/fcmpushnotification/dto/UserDTO.java
+++ src/main/java/com/anuradha/fcmpushnotification/dto/UserDTO.java
@@ -12,5 +12,55 @@ public class UserDTO implements Serializable {
     private String deviceToken;
     private List<Topic> topics;
 
+    public UserDTO() {
+    }
 
+    public UserDTO(int user_id, String name, String deviceToken, List<Topic> topics) {
+        this.setUser_id(user_id);
+        this.setName(name);
+        this.setDeviceToken(deviceToken);
+        this.setTopics(topics);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "user_id=" + getUser_id() +
+                ", name='" + getName() + '\'' +
+                ", deviceToken='" + getDeviceToken() + '\'' +
+                ", topics=" + getTopics() +
+                '}';
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeviceToken() {
+        return deviceToken;
+    }
+
+    public void setDeviceToken(String deviceToken) {
+        this.deviceToken = deviceToken;
+    }
+
+    public List<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }
 }
