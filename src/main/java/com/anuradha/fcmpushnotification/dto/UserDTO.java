24e04a7a05746b189bcfd5f0f005b2d3752aed29
anuradha151 <anuranasinghe151@gmail.com>
2018-10-11 11:59:58 +0530

    topic dto implemented serializable interface

--- src/main/java/com/anuradha/fcmpushnotification/dto/UserDTO.java
+++ src/main/java/com/anuradha/fcmpushnotification/dto/UserDTO.java
@@ -1,6 +1,16 @@
 package com.anuradha.fcmpushnotification.dto;
 
+import com.anuradha.fcmpushnotification.model.Topic;
+
 import java.io.Serializable;
+import java.util.List;
 
 public class UserDTO implements Serializable {
+
+    private int user_id;
+    private String name;
+    private String deviceToken;
+    private List<Topic> topics;
+
+
 }
